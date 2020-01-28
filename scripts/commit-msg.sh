7ab3a36bdd98c85395c0475f0fe4bf67df74a705
Kirill Litvinov <k.litvinov.spdload@gmail.com>
2020-01-28 18:28:54 +0200

    Commit msg prepending

---
+++ scripts/commit-msg.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BRANCHES_TO_SKIP=("master" "develop" "test")
+
+BRANCH_NAME=$(git symbolic-ref --short HEAD)
+
+BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
+BRANCH_IN_COMMIT=$(grep -c "\[$BRANCH_NAME\]" $1)
+
+if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then
+    xargs -r sed -i.bak -e "1s%^%[$BRANCH_NAME] %" $1
+fi
